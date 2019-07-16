2c1cc36d3c22cda1f03b98057f9483aac0e13137
usmanriaz111 <l124061@lhr.nu.edu.pk>
2019-07-16 20:40:12 +0500

    Updated the pdf view and price

--- app/controllers/spree/admin/invoice_controller.rb
+++ app/controllers/spree/admin/invoice_controller.rb
@@ -7,12 +7,11 @@ module Spree
         # instance_variable_set('@' + template, true)
         @order = Spree::Order.find_by(number: params[:id])
         # render template, layout: false
-
         respond_to do |format|
           format.html
           format.pdf do
-            @pdf = render_to_string :pdf => 'invoice',:zoom => '0.78125',:template => 'spree/admin/invoice/show.html.erb', :encoding => "UTF-8"
-            send_data(@pdf, :filename => 'invoice.pdf', :type=>'application/pdf')
+            @pdf = render_to_string :pdf => 'Black-Line-OrderInvoice',:zoom => '0.78125',:template => 'spree/admin/invoice/show.html.erb', :encoding => "UTF-8"
+            send_data(@pdf, :filename => 'Black-Line-OrderInvoice.pdf', :type=>'application/pdf')
           end
         end
 
